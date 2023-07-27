074f789a988b672cd0a2ec5d2247d8636596534e
Bruno Santos <bruno4t@hotmail.com>
2023-07-27 15:22:34 -0300

    Criação do projeto

---
+++ src/test/java/com/MyBlog/Security/BlogSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyBlog.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
