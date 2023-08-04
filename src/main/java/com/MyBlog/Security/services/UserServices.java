e2ad4e1748082858c441971e7ffcd57c2f83b182
Bruno Santos <bruno4t@hotmail.com>
2023-08-04 16:05:09 -0300

    Inserção das primeias logicas de negocio, faltando a implementação

--- src/main/java/com/MyBlog/Security/services/UserServices.java
+++ src/main/java/com/MyBlog/Security/services/UserServices.java
@@ -1,7 +1,10 @@
 package com.MyBlog.Security.services;
 
+import com.MyBlog.Security.dto.UserCadasterDto;
+import com.MyBlog.Security.dto.UserDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserServices {
+    UserDTO registerNewUser(UserCadasterDto usuarioCadastro);
 }
