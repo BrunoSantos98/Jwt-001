d860809590aadaff4b1627102b881e7298ef91e5
Bruno Santos <bruno4t@hotmail.com>
2023-08-04 15:43:46 -0300

    final da implementação de userdetails

--- src/main/java/com/MyBlog/Security/configs/security/UserDetailsServicesImpl.java
+++ src/main/java/com/MyBlog/Security/configs/security/UserDetailsServicesImpl.java
@@ -1,10 +1,18 @@
 package com.MyBlog.Security.configs.security;
 
+import com.MyBlog.Security.exceptions.ObjectNotFoundException;
+import com.MyBlog.Security.models.User;
 import com.MyBlog.Security.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@Transactional
 public class UserDetailsServicesImpl implements UserDetailsService {
 
     private final UserRepository repository;
@@ -15,6 +23,11 @@ public class UserDetailsServicesImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<User> user = Optional.of(repository.findByUsername(username));
+        if(user.isEmpty()){
+            throw new ObjectNotFoundException("Nome de usuario não encontrado na base de dados");
+        }
+        return new org.springframework.security.core.userdetails.User(user.get().getUsername(),
+                user.get().getPassword(), user.get().getAuthorities());
     }
 }
